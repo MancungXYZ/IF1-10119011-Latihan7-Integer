acb94ce2ed68e74ab3bf4fa16acdd78610ffd110
Reihan Wiyanda <Reihan Wiyanda@Mancungs>
2020-10-17 11:34:47 +0700

    IF1-10119011-Latihan7-Integer

---
+++ src/if1/pkg10119011/latihan7/integer/IF110119011Latihan7Integer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119011.latihan7.integer;
+
+/**
+ *
+ * @author 
+ *  Nama    : Reihan Wiyanda
+ *  Kelas   : IF-1
+ *  Nim     : 10119011
+ */
+public class IF110119011Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       byte b;
+       short s;
+       int i;
+       long l;
+       b = 120;
+       s = 32767;
+       i = 1_000_000_000;
+       l = 9000000000L;
+       System.out.println("byte : " + b);
+       System.out.println("short : " + s);
+       System.out.println("int : " + i);
+       System.out.println("long : " + l);
+       
+    }
+    
+}
